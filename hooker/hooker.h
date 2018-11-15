552ad69e76605fd04042f390432f25152930d929
subdiox <subdiox@gmail.com>
2018-11-16 04:03:06 +0900

    Initial Commit

---
+++ hooker/hooker.h
@@ -0,0 +1,13 @@
+//
+//  hooker.h
+//  hooker
+//
+//  Created by subdiox on 2018/11/16.
+//  Copyright © 2018 subdiox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface hooker : NSObject
+
+@end
---
+++ hooker/hooker.m
@@ -0,0 +1,13 @@
+//
+//  hooker.m
+//  hooker
+//
+//  Created by subdiox on 2018/11/16.
+//  Copyright © 2018 subdiox. All rights reserved.
+//
+
+#import "hooker.h"
+
+@implementation hooker
+
+@end
